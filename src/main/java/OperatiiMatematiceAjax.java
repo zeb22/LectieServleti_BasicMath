b6159a5bb96fcabe201b8b8738216609b3ea3669
Seb <zeb.2202@gmail.com>
2016-06-23 13:45:44 +0300

    merge calcul traditional cu 3 numere, nu merge cu ajax, nu scrie in DB

--- src/main/java/OperatiiMatematiceAjax.java
+++ src/main/java/OperatiiMatematiceAjax.java
@@ -13,32 +13,34 @@ public class OperatiiMatematiceAjax extends HttpServlet {
 
         PrintWriter out =resp.getWriter();
 
-        String sNr1=req.getParameter("nr1");
-        String sNr2=req.getParameter("nr2");
-        String sOp=req.getParameter("op");
+        String sNr1 = req.getParameter("nr1");
+        String sNr2 = req.getParameter("nr2");
+        String sNr3 = req.getParameter("nr3");
+        String sOp = req.getParameter("op");
 
-        int nr1=Integer.parseInt(sNr1);
-        int nr2=Integer.parseInt(sNr2);
-        double resultValue=0;
+        int nr1 = Integer.parseInt(sNr1);
+        int nr2 = Integer.parseInt(sNr2);
+        int nr3 = Integer.parseInt(sNr3);
+        double resultValue = 0;
 
         switch (sOp) {
             case "1":
-                resultValue = nr1 + nr2;
+                resultValue = nr1 + nr2 + nr3;
                 break;
             case "2":
-                resultValue = nr1 - nr2;
+                resultValue = nr1 - nr2 - nr3;
                 break;
             case "3":
-                resultValue = nr1 * nr2;
+                resultValue = nr1 * nr2 * nr3;
                 break;
             case "4":
-                resultValue = (double) nr1 / nr2;
+                resultValue = (double) nr1 / (nr2 * nr3);
                 break;
         }
 
 
 
-        out.println("result is here: <b>"+resultValue+"</b>");
+        out.println("result is here: <b>" + resultValue + "</b>");
 
         out.close();
 
--- src/main/java/OperatiiMatematiceTraditional.java
+++ src/main/java/OperatiiMatematiceTraditional.java
@@ -14,47 +14,46 @@ public class OperatiiMatematiceTraditional extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         resp.setContentType("text/html;charset=UTF-8");
-        PrintWriter out =resp.getWriter();
+        PrintWriter out = resp.getWriter();
 
-        String sNr1=req.getParameter("nr1");
-        String sNr2=req.getParameter("nr2");
-        String sOp=req.getParameter("op");
+        String sNr1 = req.getParameter("nr1");
+        String sNr2 = req.getParameter("nr2");
+        String sNr3 = req.getParameter("nr3");
+        String sOp = req.getParameter("op");
 
-        int nr1=Integer.parseInt(sNr1);
-        int nr2=Integer.parseInt(sNr2);
-        double resultValue=0;
+        int nr1 = Integer.parseInt(sNr1);
+        int nr2 = Integer.parseInt(sNr2);
+        int nr3 = Integer.parseInt(sNr3);
+        double resultValue = 0;
 
         switch (sOp) {
             case "1":
-                resultValue = nr1 + nr2;
+                resultValue = nr1 + nr2 + nr3;
                 break;
             case "2":
-                resultValue = nr1 - nr2;
+                resultValue = nr1 - nr2 - nr3;
                 break;
             case "3":
-                resultValue = nr1 * nr2;
+                resultValue = nr1 * nr2 * nr3;
                 break;
             case "4":
-                resultValue = (double) nr1 / nr2;
+                resultValue = (double) nr1 / (nr2 * nr3);
                 break;
         }
 
-
-
         out.println("<html>");
         out.println("<head>");
-        out.println("<title>Calcule </title>");
+        out.println("<title>Calcule</title>");
         out.println("</head>");
-
         out.println("<body>");
-
-
-        out.println("result is: <b>"+resultValue+"</b>");
-
+        out.println("Result is: <b>" + resultValue + "</b>");
         out.close();
 
-
-
         out.println("</body>");
+
+        //insert to DB
+
+        DB d = new DB();
+        d.insert(resultValue);
     }
 }
